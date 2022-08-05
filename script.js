8c53315e77786714cb3268af09f2d70eddc6652b
denis <denisvictorjosphine1996@gmail.com>
2022-08-05 11:53:38 +0530

    add headings and write restart function and display function

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 (function(){
- const player = function ( sign) {
-     return {sign};
+
+  
+
+ const player = function ( name,sign) {
+     return {name,sign};
     }
 
   
 const gameBoard = (() =>{
-  var arr = [['','',''],
-             ['','',''],
-             ['','',''] ];
 
-const setBoard = (x,y,player) =>{
-arr[x][y] = player.sign; 
-if(win(player)){
-console.log('wins');
+
+var arr = ['','','','','','','','',''];
+
+const setBoard = (position,player) =>{
+arr[position] = player.sign; 
+
+if(draw()){
+  display(player1,player2);
+}
+else if(win(player)){
+display(player,null);
 }
 }
+
 
 const draw = () =>{
   for(let i =0; i<9; i++){
@@ -25,83 +33,126 @@ const draw = () =>{
   return true;
 }
 
+
+
 const resetBoard = () =>{
   for(let i =0; i<9; i++){
     arr[i] = '';
   }
 }
 
+
 const win = (player) =>{
-  let n =0;
-  for(let i=0; i<3; i++){
-    for(let j =0; j<3; j++){
-      if(arr[i][j] === player.sign){
-        n++;
-      }
-    }
-    if(n === 3){
-      return true
-    }else{
-      n =0;
-    }
-  }
- 
-
-  n=0;
-  for(let i=0; i<3; i++){
-    for(let j =0; j<3; j++){
-      if(arr[j][i] === player.sign){
-        n++;
-      }
-    }
-    if(n === 3){
-      return true
-    }else{
-      n =0;
-    }
-  }
- 
-
-  n = 0;
-  for(let i =j =0; i<3 && j<3; i++, j++){
-    if(arr[i][j] === player.sign){
-      n++;
-    }
-  }
-  if(n === 3){
-    return true;
-  }
-
- 
-  n =0;
-
-  for(let i =j =0; i<3 && j<3; i++, j++){
-    if(arr[i][2-j] === player.sign){
-      n++;
-    }
-  }
-  if(n === 3){
-    return true;
-  }
-  
-  n =0;
-
-return false;
+  const winArray = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6] ]
+ return winArray.some(combo=>{
+  return combo.every(index=>{
+    return arr[index] === player.sign;
+  });
+ })
 }
 
 
 
-return {setBoard, resetBoard, draw,win};
+return {setBoard, resetBoard};
+
+})()
+
+function display(obj1,obj2){
+  let winning_message = document.querySelector('.winning-message');
+  winning_message.style.display = 'flex'
+  if(obj2 === null){
+  document.querySelector('.message').textContent =   `${obj1.name} wins!!`;
+  
+  }else{
+
+    document.querySelector('.message').textContent = 'Its a draw try again!'
+    
+  }
+}
+
+
+const player1 = player('player 1','x');
+const player2 = player('player 2','o');
+
+function toggleBoard(){
+ 
+  if(x_class){
+    board.classList.remove('x');
+    board.classList.add('circle');
+     x_class = false;
+  }else{
+    board.classList.remove('circle');
+    board.classList.add('x');
+    x_class = true;
+  }
+}
+
+
+var x_class = true;
+var board =document.querySelector('.board')
+board.addEventListener('click',toggleBoard);
+
+var cell_x = false;
+var cells = document.querySelectorAll('.cell');
+cells.forEach(cell=>cell.addEventListener('click',function(e){
+  let position = e.target.id;
+  if(cell_x){
+    gameBoard.setBoard(position,player1);
+  }else{
+    gameBoard.setBoard(position,player2);
+  }
+  
+
+
+  if(cell_x){
+e.target.classList.remove('x');
+e.target.classList.add('circle');
+cell_x = false;
+}else{
+  e.target.classList.remove('circle');
+  e.target.classList.add('x');
+  cell_x = true;
+}
+
+},{once:true}));
+
+
+document.querySelector('.restart').addEventListener('click',function(){
+  location.reload();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })()
 
 
 
-const player1 = player('x');
-const player2 = player('o');
-gameBoard.setBoard(0,0,player1);
-gameBoard.setBoard(1,1,player1);
-gameBoard.setBoard(2,2,player1);
 
 
 
@@ -110,25 +161,3 @@ gameBoard.setBoard(2,2,player1);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})()
